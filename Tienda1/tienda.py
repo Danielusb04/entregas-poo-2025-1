e49724c020503e6f369bee886b28135a63e59667
Danielusb04 <danielgmh040404@gmail.com>
2025-02-28 15:39:05 -0500

    subir

--- Tienda1/tienda.py
+++ Tienda1/tienda.py
@@ -12,6 +12,7 @@ Autor: Daniel Gustavo Montañez Hurtado
 Fecha: 2025-02-25
 """
 
+
 class Product:
     def __init__(self, name, price_per_unit, amount):
         self.name = name
@@ -21,11 +22,13 @@ class Product:
     def __str__(self):
         return f' - {self.name:<10} - {self.price_per_unit:<12} - {self.amount:<10} cop'
 
+
 def get_product(num_product):
     name = input(f'Product {num_product}, what is the product name? ')
     price_per_unit = float(input(f'What is the price of {name}? '))
     amount = int(input(f'How many {name} do we have? '))
     return Product(name, price_per_unit, amount)
+
 
 def show_summary(products):
     print("\nSummary:")
@@ -33,12 +36,14 @@ def show_summary(products):
     for product in products:
         print(product)
 
+
 def main():
     products = []
     for i in range(1, 4):
         product = get_product(i)
         products.append(product)
     show_summary(products)
+
 
 if __name__ == "__main__":
     main()
