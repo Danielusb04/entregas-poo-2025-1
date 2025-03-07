8a20a006c92cf5fcf846b7cd3b2bd7fb176b117f
Danielusb04 <danielgmh040404@gmail.com>
2025-03-06 19:50:44 -0500

    Add files via upload

---
+++ Tienda2/entregas-poo-2025-1/Tienda1/tienda.py
@@ -0,0 +1,71 @@
+"""
+Practica1: Hacer un programa que maneje un inventario
+de una tienda que devuelva el nombre del producto, cantidad y precio.
+
+Por medio del siguiente programa lograremos
+hacer un programa que nos ayude a identificar
+el tipo de producto que tenemos en una tienda,
+su precio y cantidad.
+
+Autor: Daniel Gustavo Montañez Hurtado
+<dgmontanezh@academia.usbbog.edu.co>
+Fecha: 2025-02-25
+"""
+
+
+class Product:
+    def __init__(self, name, price_per_unit, amount, description, category): # we creat the class and the init constructure get the values
+        self.name = name
+        self.price_per_unit = price_per_unit
+        self.amount = amount
+        self.description = description
+        self.category = category
+
+
+    def __str__(self):  # it rerund a lash with the name, ammount and price
+        return f' - {self.name:<10} - {self.amount:<12} - {self.price_per_unit:<10} cop - {self.category:<10} - {self.description}'
+    
+    def total_price(self):
+        return self.price_per_unit * self.amount
+ 
+
+def get_product(num_product): # here is where we reuqest to the user the information
+    name = input(f'Product {num_product}, what is the product name? ')
+    price_per_unit = float(input(f'What is the price of {name}? '))
+    amount = int(input(f'How many {name} do we have? '))
+    description = input((f'pleasde describe {name}'))
+    category = input(f'What is the category name for {name}?')
+    return Product(name, price_per_unit, amount, description, category)
+
+
+def show_summary(products): # show summary helps us to creat the table
+    print("\nSummary:")
+    print(f"{'Product':<10}{'amount':<12}{'price per unit':<20}{'category':<20}{'description'}")
+    for product in products: 
+        print(product)
+
+def show_category_summary(products):
+    categories = {}
+    for product in products:
+        if product.category not in categories:
+            categories[product.category] = 0
+        categories[product.category] +=product.total_price()
+
+    print("\nCategory summary:")
+    for category, total_price in categories.items():
+        print(f'category: {category} - total price: {total_price:.2f} cop')
+
+
+def main(): # with the main i create a blank list where data would be save
+    num_products = int(input("How many products would you like to enter?"))
+    products = []
+    for i in range(1, num_products + 1):
+        product = get_product(i)
+        products.append(product)
+    
+    show_summary(products)
+    show_category_summary(products)
+
+if __name__ == "__main__": #excute this code
+    main()
+
